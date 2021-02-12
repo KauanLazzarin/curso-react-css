b3599a9b046da205d976df74ff8ff8b93bc3cda6
Kauan Lazzarin <kauanlazzarin@gmail.com>
2021-02-12 16:23:02 -0300

    primeira aula sobre animações finalizada

---
+++ src/Animations.js
@@ -0,0 +1,8 @@
+export default function Animations () {
+    return (
+        <div className="animeLeft">
+            <h1>Este componente está sendo animado</h1>
+            <small><i>A página inteira está...</i></small>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import './css/App.css';
+import React from 'react';
+import Animations from './Animations';
 import CssModules from './CssModules';
 import Frameworks from './Frameworks';
 import StyledComponents from './StyledComponents';
+import './css/App.css';
 
 export default function App() {
+  const [isAtivo, setAtivo] = React.useState(false);
+  
   return (
-    <div className="">
+    <div className="animeLeft">
       <h1>CSS com React</h1>
+
 
       <CssModules />
       <StyledComponents />
       <Frameworks />
+      
+      <button onClick={() => setAtivo(!isAtivo)}>Ativar componente animado</button>
+      {isAtivo && <Animations />}
     </div>
   );
 }
